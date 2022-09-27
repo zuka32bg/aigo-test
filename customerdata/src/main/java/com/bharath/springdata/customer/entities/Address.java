52e34085a69dd3531de219a34c8cac5de2c2f465
uros.zukancic <uros.zukancic@aigo.rs>
2022-09-27 12:50:16 +0200

    Changed state2 to country, fix problem

--- customerdata/src/main/java/com/bharath/springdata/customer/entities/Address.java
+++ customerdata/src/main/java/com/bharath/springdata/customer/entities/Address.java
@@ -9,7 +9,7 @@ public class Address {
 	private String city;
 	private String state;
 	private String zipcode;
-	private String state2;
+	private String country;
 
 	public String getCity() {
 		return city;
