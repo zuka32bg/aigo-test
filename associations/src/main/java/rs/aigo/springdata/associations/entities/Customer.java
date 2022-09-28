b25965b2fb5b0f8f942e0b91d6d40f4fe31c4ac5
uros.zukancic <uros.zukancic@aigo.rs>
2022-09-28 11:29:11 +0200

    Created CustomerRepository Commit before test

--- associations/src/main/java/rs/aigo/springdata/associations/entities/Customer.java
+++ associations/src/main/java/rs/aigo/springdata/associations/entities/Customer.java
@@ -8,28 +8,37 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-
 @Entity
 public class Customer {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	
-	@OneToMany(mappedBy = "customer" )
-	private Set <PhoneNumber> numbers;
-	
+	private String name;
+	@OneToMany(mappedBy = "customer")
+	private Set<PhoneNumber> numbers;
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
 
-	public Set <PhoneNumber> getNumbers() {
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<PhoneNumber> getNumbers() {
 		return numbers;
 	}
-	public void setNumbers(Set <PhoneNumber> numbers) {
+
+	public void setNumbers(Set<PhoneNumber> numbers) {
 		this.numbers = numbers;
 	}
-	
+
 }
